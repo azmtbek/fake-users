9f5929b9d9d75e10f73eddc23a94e1a8dc1c05eb
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-17 22:22:47 +0500

    remove unused files

--- src/app/api/data/route.ts
+++
@@ -1,6 +0,0 @@
-export async function POST(request: Request) {
-  const formData = await request.formData();
-  const region = formData.get('region');
-  const seed = formData.get('seed');
-  return Response.json({ region, seed });
-}
